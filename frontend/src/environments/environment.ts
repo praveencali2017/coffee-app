2bd70bf90884e334e7bedf42cec03b75ec60789e
unknown <praveenknjun12@gmail.com>
2020-11-08 18:47:50 -0800

    working proj with auth0 configured

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'https://dev-prav-auth.us.auth0.com/authorize', // the auth0 domain prefix
+    url: 'dev-prav-auth.us', // the auth0 domain prefix
     audience: 'user', // the audience set for the auth0 app
     clientId: 'rFcvzl2ANICAOX6CGma6GP9YIX6eyGAT', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
